a9637cba46c04b5e47ef59324dc250c7755da41f
Elijah Cordova <ejcordov@ucsc.edu>
2017-01-10 16:42:25 -0800

    added helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//-----------------------------------------------------
+// HelloWorld.java
+// Prints "Hello world", the first program you should always write
+//-----------------------------------------------------
+class HelloWorld {
+    static public void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
