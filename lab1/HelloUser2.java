f82c28e9d6b814af7433da4601df76ed99b2337e
Elijah Cordova <ejcordov@ucsc.edu>
2017-01-16 20:23:53 -0800

    Added lab1 files.

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------------------------
+// HelloUser2.java
+// Prints greeting Hello + User name
+//-----------------------------------------------------------------------------
+class HelloUser2{
+   public static void main( String[] args ){
+      String userName = System.getProperty("user.name");
+       System.out.println("Hello "+userName);
+      }
+}
